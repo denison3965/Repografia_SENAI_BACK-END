77b5dacf822feba3c908972d1a16afcbed1202d4
Denison Portela <denisondealmeidaprtela@hotmail.com>
2020-12-08 23:05:27 -0300

    Faezendo auto incremente no id_requisicao

---
+++ src/controllers/VerifyJWT-controller.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+const app = express(); 
+const bcrypt = require('bcrypt');
+
+
+require("dotenv-safe").config();
+const jwt = require('jsonwebtoken');
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+exports.get = ( req, res, next ) => {
+    // res.send("Esta tudo funcionando")
+    console.log("Esta tudo funcionando");
+
+    //Se o usuario for Adiministrador envias isso
+        // res.json([{id:1,nome:'luiz',auth: true, adm: true}]);
+    //else    
+        res.json([{id: req.userId ,nome:'luiz',auth: true, adm: true}]);
+}
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const app = express(); 
+const app = express();
 
 
 require("dotenv-safe").config();
@@ -12,69 +12,140 @@ const mysql = require('./model/db').pool
 //Chamando as controllers
 const Login_controller = require('./controllers/Login-controller')
 const Logout_controller = require('./controllers/Logout-controller')
+const verifyJWT_controller = require('./controllers/VerifyJWT-controller')
 
 
 // Verificar o JWT
-function verifyJWT(req, res, next){
+function verifyJWT(req, res, next) {
     const token = req.headers['x-access-token'];
     if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
-    
-    jwt.verify(token, process.env.SECRET, function(err, decoded) {
-      if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
-      
-      // se tudo estiver ok, salva no request para uso posterior
-      req.userId = decoded.id;
-      next();
+
+    jwt.verify(token, process.env.SECRET, function (err, decoded) {
+        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+
+        // se tudo estiver ok, salva no request para uso posterior
+        req.userId = decoded.id;
+        next();
     });
 }
 
 //Routers
-    router.get('/teste', verifyJWT, ( req, res, next ) => {
-        // res.send("Esta tudo funcionando")
-        console.log("Esta tudo funcionando");
+router.get('/teste', verifyJWT, verifyJWT_controller.get)
 
-        //Se o usuario for Adiministrador envias isso
-            // res.json([{id:1,nome:'luiz',auth: true, adm: true}]);
-        //else    
-            res.json([{id: req.userId ,nome:'luiz',auth: true, adm: true}]);
+
+//Exemplo Excluir depois
+router.post('/add-fornecedor', (req, res) => {
+
+
+
+    mysql.getConnection((error, conn) => {
+
+        if (error) res.status(500).send({ error: error })
+
+        conn.query(
+            'INSERT INTO fornecedor (id_fornecedor, nome) VALUES (?,?)',
+            [req.body.id, req.body.nome],
+            (error, resultado, field) => {
+                conn.release()
+
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    })
+                }
+                res.status(201).send({
+                    mesagem: 'Fornecedor inserido com sucesso',
+                })
+            }
+        )
     })
 
 
-    //Exemplo Excluir depois
-    router.post('/add-fornecedor', ( req, res ) => {
+})
 
-       mysql.getCo+nnection((error, conn) => {
+//Exemplo Excluir depois
+router.post('/add-requisicao', (req, res) => {
 
-           if(error) res.status(500).send({error: error})
 
-           conn.query(
-               'INSERT INTO fornecedor (id_fornecedor, nome) VALUES (?,?)',
-               [5, 'Meu teste com MySql'],
-               (error, resultado, field) => {
-                   conn.release()
+    mysql.getConnection((error, conn) => {
 
-                   if(error) {
-                       return res.status(500).send({
-                           error: error,
-                           response: null
-                       })
-                   }
-                   res.status(201).send({
-                       mesagem: 'Fornecedor inserido com sucesso',
-                   })
-               }
-           )
-       })
-       
+        if (error) res.status(500).send({ error: error })
 
+        //Gerando numero da requisicao
+            //pegar o ultimo numero de registro
+            var year = new Date().getFullYear();
+
+            //Consultar o ultimo registro e acrescentar mais um numero
+            let num = null;
+            let numero_requisicao
+
+            conn.query('SELECT id_requisicao from requisicao ORDER BY id_requisicao DESC limit 1',
+                (error, resultado, field) =>{
+                    
+                    num = resultado
+                    num =  num[0].id_requisicao
+                    num = num.toString()
+
+                    console.log(num)    
+
+                    if(num != null) 
+                    {
+                        console.log("estou aqui")
+                        num = num.substr(4)
+                        num = num + 1
+                        numero_requisicao = parseInt(`${year}${num}`)
+                    }
+                    else
+                    {
+                        console.log("estou aqui 2")
+                        num = 1
+                        numero_requisicao = parseInt(`${year}${num}`)
+                    }
+
+                })
+
+
+
+
+        conn.query(
+            'INSERT INTO requisicao (id_requisicao, NIF, num_paginas, num_copias, total_paginas, observacao, data_envio, data_entrega ) VALUES (?,?,?,?,?,?,?,?)',
+            [
+                numero_requisicao, 
+                511128,
+                req.body.paginas,
+                req.body.copias,
+                req.body.totalPaginas,
+                req.body.observacao,
+                req.body.dataSolicitante,
+                req.body.dataEntrega
+            ],
+
+            (error, resultado, field) => {
+                conn.release()
+
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    })
+                }
+                res.status(201).send({
+                    mesagem: 'Requisicao criada com sucesso !!!',
+                })
+            }
+        )
     })
 
-    //autenticação
-    router.post('/login', Login_controller.post)
 
-    router.post('/logout',Logout_controller.post)
+})
 
-    
+//autenticação
+router.post('/login', Login_controller.post)
+
+router.post('/logout', Logout_controller.post)
+
+
 
 
 
