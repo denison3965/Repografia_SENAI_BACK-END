1797b5fae315b4275ccee4f6caa7fee1fd8c5872
ana204 <anagomes.lucia@hotmail.com>
2020-12-09 15:22:21 -0300

    subindo a rota funcionario correto

--- src/controllers/Funcionarios_controller.js
+++ src/controllers/Funcionarios_controller.js
@@ -18,8 +18,8 @@ exports.post = (req, res) => {
 
     
     conn.query(
-        'INSERT INTO funcionarios ( nif, nome, sobrenome, email, telefone ,data_criacao, senha, perfil, situacao, data_suspensao) VALUES (?,?,?,?,?,?,?,?,?,?)',
-        [req.body.nif, req.body.nome, req.body.sobrenome,req.body.email, telefone,req.body.data_criacao,req.body.senha,req.body.perfil,req.body.situacao, req.body.data_suspensao],
+        'INSERT INTO funcionarios ( nif, nome, sobrenome, email, telefone ,data_criacao, senha, administrativo, situacao, data_suspensao, id_cargo) VALUES (?,?,?,?,?,?,?,?,?,?)',
+        [req.body.nif, req.body.nome, req.body.sobrenome,req.body.email, telefone,req.body.data_criacao,req.body.senha,req.body.administrativo,req.body.situacao, req.body.data_suspensao],
         (error, resultado, field) => {
             conn.release()
 
