a2fe9e1e48e8cfbeab8a7faad35490ff50afc785
Denison Portela <denisondealmeidaprtela@hotmail.com>
2020-12-09 14:52:42 -0300

    Adicionando criptografia na senha

--- src/controllers/Funcionarios_controller.js
+++ src/controllers/Funcionarios_controller.js
@@ -1,40 +1,45 @@
 const express = require('express')
 const router = express.Router()
-const app = express(); 
+const app = express();
 require("dotenv-safe").config();
 const jwt = require('jsonwebtoken');
 const mysql = require('../model/db').pool
+const bcrypt = require('bcrypt');
 
 
 exports.post = (req, res) => {
     mysql.getConnection((error, conn) => {
-    if(error) res.status(500).send({error: error})
-        
-    //sentando o numero do senai padrão caso o usuario não coloque numero
-    var telefone = '01010101'
-    if(req.body.telefone != null ){
-        telefone = req.body.telefone
-    }
+        if (error) res.status(500).send({ error: error })
 
-    
-    conn.query(
-        'INSERT INTO funcionarios ( nif, nome, sobrenome, email, telefone ,data_criacao, senha, perfil, situacao, data_suspensao) VALUES (?,?,?,?,?,?,?,?,?,?)',
-        [req.body.nif, req.body.nome, req.body.sobrenome,req.body.email, telefone,req.body.data_criacao,req.body.senha,req.body.perfil,req.body.situacao, req.body.data_suspensao],
-        (error, resultado, field) => {
-            conn.release()
-
-            if(error) {
-                return res.status(500).send({
-                    error: error,
-                    response: null
-                })
-            }
-            res.status(201).send({
-                mesagem: 'Funcionario inserido com sucesso',
-                id_funcionarios: resultado.insertId
-            })
+        //sentando o numero do senai padrão caso o usuario não coloque numero
+        var telefone = '01010101'
+        if (req.body.telefone != null) {
+            telefone = req.body.telefone
         }
-    )
+
+        bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
+            if (errBcrypt) { return res.status(500).send({ error: errorBcrypt }) }
+            conn.query(
+                'INSERT INTO funcionarios ( nif, nome, sobrenome, email, telefone ,data_criacao, senha, perfil, situacao, data_suspensao) VALUES (?,?,?,?,?,?,?,?,?,?)',
+                [req.body.nif, req.body.nome, req.body.sobrenome, req.body.email, telefone, req.body.data_criacao, req.body.senha, req.body.perfil, req.body.situacao, req.body.data_suspensao],
+                (error, resultado, field) => {
+                    conn.release()
+
+                    if (error) {
+                        return res.status(500).send({
+                            error: error,
+                            response: null
+                        })
+                    }
+                    res.status(201).send({
+                        mesagem: 'Funcionario inserido com sucesso',
+                        id_funcionarios: resultado.insertId
+                    })
+                }
+            )
+
+        })
+
 
     })
 }
