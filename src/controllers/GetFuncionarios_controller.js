d727d6821dde4f2e4ac38c934c916c24a6311a38
Denison Portela <denisondealmeidaprtela@hotmail.com>
2020-12-11 20:34:17 -0300

    Subindo listagem de pagina

---
+++ src/controllers/GetFuncionarios_controller.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const app = express();
+require("dotenv-safe").config();
+const jwt = require('jsonwebtoken');
+const mysql = require('../model/db').pool
+const bcrypt = require('bcrypt');
+
+exports.get = (req , res) => {
+    
+    mysql.getConnection((error, conn) => {
+        if (error) res.status(500).send({ error: error })
+
+        conn.query('SELECT id_funcionarios, nif, nome, sobrenome, email, telefone, data_criacao, administrativo, situacao, data_suspensao, id_cargo FROM funcionarios',
+            (err, result) => {
+                conn.release()
+
+                if (err) {
+                    return res.status(500).send({
+                        error: err,
+                        response: null
+                    })
+                }
+
+                res.send(result)
+            })
+    })
+}
