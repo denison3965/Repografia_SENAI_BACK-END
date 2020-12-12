f564737af6b792bb06f83a514c96a3a5a8d93be4
ana204 <anagomes.lucia@hotmail.com>
2020-12-11 22:52:06 -0300

    db.js

--- src/controllers/EditarSenha_controller.js
+++ src/controllers/EditarSenha_controller.js
@@ -7,33 +7,39 @@ const bcrypt = require('bcrypt');
 
 
 exports.put = (req, res) => {
-    mysql.getConnection((error, conn) => {
-        if(error) res.status(500).send({error: error})
 
-        bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
-            if (errBcrypt) { return res.status(500).send({ error: errorBcrypt }) }
-            conn.query(
-                `UPDATE funcionarios
-                  SET senha   = ?
-                  WHERE nif   = ? `,
+    res.send("Senha atual: "+req.body.senhaAtual + "Nova Senha: "+req.body.novaSenha + "Confirmar senha: "+req.body.confirmarSenha)
+
+    console.log(req.body.senhaAtual)
+    console.log(req.body.novaSenha)
+    console.log(req.body.confirmarSenha)
+    // mysql.getConnection((error, conn) => {
+    //     if(error) res.status(500).send({error: error})
+
+    //     bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
+    //         if (errBcrypt) { return res.status(500).send({ error: errorBcrypt }) }
+    //         conn.query(
+    //             `UPDATE funcionarios
+    //               SET senha   = ?
+    //               WHERE nif   = ? `,
             
-                  [hash, req.body.nif],
+    //               [hash, req.body.nif],
             
-                  (error, resultado, field) => {
-                   conn.release()
-                   if(error) {
-                       return res.status(500).send({
-                           error: error,
-                           response: null
-                       })
-                   }
-                   res.status(201).send({
-                       mesagem: 'Senha alterada com sucesso',
-                   })
-               }
-               )
-        })
-    })
+    //               (error, resultado, field) => {
+    //                conn.release()
+    //                if(error) {
+    //                    return res.status(500).send({
+    //                        error: error,
+    //                        response: null
+    //                    })
+    //                }
+    //                res.status(201).send({
+    //                    mesagem: 'Senha alterada com sucesso',
+    //                })
+    //            }
+    //            )
+    //     })
+    // })
 }
 
 
