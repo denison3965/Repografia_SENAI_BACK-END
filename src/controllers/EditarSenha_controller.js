17b48c234e934502db5ff1d812468746f0c4725b
ana204 <anagomes.lucia@hotmail.com>
2020-12-13 20:58:21 -0300

    subindo logica de confimarcao de senha

--- src/controllers/EditarSenha_controller.js
+++ src/controllers/EditarSenha_controller.js
@@ -7,39 +7,66 @@ const bcrypt = require('bcrypt');
 
 
 exports.put = (req, res) => {
+   
+    mysql.getConnection((error, conn) => {
+        if(error) res.status(500).send({error: error})
 
-    res.send("Senha atual: "+req.body.senhaAtual + "Nova Senha: "+req.body.novaSenha + "Confirmar senha: "+req.body.confirmarSenha)
+            //verificar se a senha atual é igual a senha que esta no banco 
+            conn.query( 
+                `SELECT senha FROM funcionarios WHERE nif = ?`,
+                [req.body.nif],
 
-    console.log(req.body.senhaAtual)
-    console.log(req.body.novaSenha)
-    console.log(req.body.confirmarSenha)
-    // mysql.getConnection((error, conn) => {
-    //     if(error) res.status(500).send({error: error})
+                async (error, resultado, field) => {
+                    if(error) {
+                        return res.status(500).send({
+                            error: error,
+                            response: null
+                        })
+                    }
 
-    //     bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
-    //         if (errBcrypt) { return res.status(500).send({ error: errorBcrypt }) }
-    //         conn.query(
-    //             `UPDATE funcionarios
-    //               SET senha   = ?
-    //               WHERE nif   = ? `,
-            
-    //               [hash, req.body.nif],
-            
-    //               (error, resultado, field) => {
-    //                conn.release()
-    //                if(error) {
-    //                    return res.status(500).send({
-    //                        error: error,
-    //                        response: null
-    //                    })
-    //                }
-    //                res.status(201).send({
-    //                    mesagem: 'Senha alterada com sucesso',
-    //                })
-    //            }
-    //            )
-    //     })
-    // })
+                    
+                    //comparar se a senha atual é igual a senha do banco de dados
+                   let verificar_senha =  await bcrypt.compareSync(req.body.senhaAtual, resultado[0].senha)
+
+                   console.log(verificar_senha)
+                   if(verificar_senha == true){
+                      //verificar se a nova senha é igual ao confimar senha 
+                      if(req.body.novaSenha === req.body.confirmarSenha){
+                          
+                        bcrypt.hash(req.body.novaSenha, 10, (errBcrypt, hash) => {
+                            if (errBcrypt) { return res.status(500).send({ error: errBcrypt }) }
+                            conn.query(
+                                `UPDATE funcionarios
+                                  SET senha   = ?
+                                  WHERE nif   = ? `,
+                            
+                                  [hash, req.body.nif],
+                            
+                                  (error, resultado, field) => {
+                                   conn.release()
+                                   if(error) {
+                                       return res.status(500).send({
+                                           error: error,
+                                           response: null
+                                       })
+                                   }
+                                   res.status(201).send('Senha alterada com sucesso')
+                               }
+                               )
+                        })
+
+                      }
+                      else{
+                          res.send("senha atual não corresponde com confirmar senha")
+                      }
+                   }
+                   else{
+                       res.send("senha atual errada")
+                   }
+                }
+                
+            )
+    })
 }
 
 
