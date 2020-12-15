e8a7d51de589d80f838778f6d424760b2d468b5a
ana204 <anagomes.lucia@hotmail.com>
2020-12-15 19:37:24 -0300

    subindo minha controller de login

--- src/controllers/Funcionarios_controller.js
+++ src/controllers/Funcionarios_controller.js
@@ -7,43 +7,69 @@ const mysql = require('../model/db').pool
 const bcrypt = require('bcrypt');
 
 
-exports.post = (req, res) => { 
+exports.post = (req, res) => {
 
-    console.log("funcionario controller")
 
     mysql.getConnection((error, conn) => {
         if (error) res.status(500).send({ error: error })
 
-        //setando o numero do senai padrão caso o usuario não coloque numero
-        let telefone = '01010101'
-        if (req.body.telefone != null) {
-            telefone = req.body.telefone
-            console.log(req.body.telefone)
-        }
+        //verificar se há um nif igual ao que já esta cadastrado 
+        conn.query(
+            '  SELECT nif FROM funcionarios WHERE nif = ?',
+            [req.body.nif],
 
-        
-        bcrypt.hash("senai115", 10, (errBcrypt, hash) => {
-            if (errBcrypt) { return res.status(500).send({ error: errorBcrypt }) }
-            conn.query(
-                'INSERT INTO funcionarios ( nif, nome, sobrenome, email, telefone ,data_criacao, senha, administrativo, situacao, data_suspensao, id_cargo) VALUES (?,?,?,?,?,?,?,?,?,?,?)',
-                [req.body.nif, req.body.nome, req.body.sobrenome, req.body.email, telefone, req.body.data_criacao, hash, administrativo, req.body.situacao, req.body.data_suspensao, req.body.id_cargo],
-                (error, resultado, field) => {
-                    conn.release()
-
-                    if (error) {
-                        return res.status(500).send({
-                            error: error,
-                            response: null
-                        })
-                    }
-                    res.status(201).send({
-                        mesagem: 'Funcionario inserido com sucesso',
-                        id_funcionarios: resultado.insertId
+            (error, resultado, field) => {
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null,
                     })
                 }
-            )
-        
-        })
+
+                let nif
+                try {
+                    nif = resultado[0].nif
+                } catch {
+                    nif = undefined
+                }
+
+                if (nif != undefined) {
+                }
+                else {
+                    //setando o numero do senai padrão caso o usuario não coloque numero
+                    let telefone = '01010101'
+                    if (req.body.telefone != null) {
+                        telefone = req.body.telefone
+                        console.log(req.body.telefone)
+                    }
+                    bcrypt.hash("senai115", 10, (errBcrypt, hash) => {
+                        if (errBcrypt) { return res.status(500).send({ error: errorBcrypt }) }
+                        conn.query(
+                            'INSERT INTO funcionarios ( nif, nome, sobrenome, email, telefone ,data_criacao, senha, administrativo, situacao, data_suspensao, id_cargo) VALUES (?,?,?,?,?,?,?,?,?,?,?)',
+                            [req.body.nif, req.body.nome, req.body.sobrenome, req.body.email, telefone, req.body.data_criacao, hash, req.body.administrativo, req.body.situacao, req.body.data_suspensao, req.body.id_cargo],
+                            (error, resultado, field) => {
+                                conn.release()
+
+                                if (error) {
+                                    return res.status(500).send({
+                                        error: error,
+                                        response: null
+                                    })
+                                }
+                                res.status(201).send({
+                                    mesagem: 'Funcionario cadastrado com sucesso!!',
+                                    id_funcionarios: resultado.insertId
+                                })
+                            }
+                        )
+
+                    })
+                }
+            }
+
+        )
+
+
 
 
 
