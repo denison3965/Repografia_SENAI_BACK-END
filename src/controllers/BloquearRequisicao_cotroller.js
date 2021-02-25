3010b86099636a6cd7025ddf6883d39fbde114b3
ana204 <anagomes.lucia@hotmail.com>
2021-02-25 16:27:44 -0300

    subindo algo para atualizar

--- src/controllers/BloquearRequisicao_cotroller.js
+++ src/controllers/BloquearRequisicao_cotroller.js
@@ -7,20 +7,13 @@ const mysql = require('../model/db').pool
 const bcrypt = require('bcrypt');
 
 
-<<<<<<< HEAD
-        conn.query('SELECT id_requisicao, nome_requisicao, num_paginas, num_copias, total_paginas, observacao, nome_arquivo, requisicao.data_entrega, requisicao.data_envio, requisicao.id_fornecedor, nome_fornecedor, requisicao.id_formato, tipo_formato, requisicao.id_suporte, tipo_suporte, requisicao.id_departamento, nome_departamento, centro_custo, funcionarios.nif, nome, status FROM requisicao INNER JOIN fornecedor ON requisicao.id_fornecedor = fornecedor.id_fornecedor INNER JOIN formato ON requisicao.id_formato = formato.id_formato INNER JOIN suporte ON requisicao.id_suporte = suporte.id_suporte INNER JOIN departamento ON requisicao.id_departamento = departamento.id_departamento INNER JOIN funcionarios ON requisicao.id_funcionarios = funcionarios.id_funcionarios WHERE funcionarios.nif = ? ORDER BY id_requisicao DESC',
-        [req.params.nif], (err, resultado, field) => {
-            conn.release()
-            res.send(resultado)
-        })
-            console.log("OLÁ! EU ESTOU AQUI!")
-=======
 exports.get = (req , res) => {
 
     mysql.getConnection((error, conn) => {
         if(error) res.status(500).res.send({error: error})
 
-        conn.query('SELECT id_requisicao, nome_requisicao, num_paginas, num_copias, total_paginas, observacao, nome_arquivo, requisicao.data_entrega, requisicao.data_envio, requisicao.id_fornecedor, nome_fornecedor, requisicao.id_formato, tipo_formato, requisicao.id_suporte, tipo_suporte, requisicao.id_departamento, nome_departamento, centro_custo, funcionarios.nif, nome, status FROM requisicao INNER JOIN fornecedor ON requisicao.id_fornecedor = fornecedor.id_fornecedor INNER JOIN formato ON requisicao.id_formato = formato.id_formato INNER JOIN suporte ON requisicao.id_suporte = suporte.id_suporte INNER JOIN departamento ON requisicao.id_departamento = departamento.id_departamento INNER JOIN funcionarios ON requisicao.id_funcionarios = funcionarios.id_funcionarios WHERE funcionarios.nif = ? ORDER BY id_requisicao DESC',[req.params.nif] , (error, result, field) => {
+        conn.query('SELECT id_requisicao, nome_requisicao, num_paginas, num_copias, total_paginas, observacao, nome_arquivo, requisicao.data_entrega, requisicao.data_envio, requisicao.id_fornecedor, nome_fornecedor, requisicao.id_formato, tipo_formato, requisicao.id_suporte, tipo_suporte, requisicao.id_departamento, nome_departamento, centro_custo, funcionarios.nif, nome, status FROM requisicao INNER JOIN fornecedor ON requisicao.id_fornecedor = fornecedor.id_fornecedor INNER JOIN formato ON requisicao.id_formato = formato.id_formato INNER JOIN suporte ON requisicao.id_suporte = suporte.id_suporte INNER JOIN departamento ON requisicao.id_departamento = departamento.id_departamento INNER JOIN funcionarios ON requisicao.id_funcionarios = funcionarios.id_funcionarios WHERE funcionarios.nif = ? ORDER BY id_requisicao DESC',
+        [req.params.nif] , (error, result, field) => {
             conn.release()
             if (error) {
                 return res.status(500).send({
@@ -28,10 +21,10 @@ exports.get = (req , res) => {
                     response: null
                 })
             }
-
-            res.send(result[0])
+            res.send(result)       
+            console.log(result)
         })
->>>>>>> 1057ba600e2cdd1395ef11a5171fe49c6d389077
+       
     })
 }
 
